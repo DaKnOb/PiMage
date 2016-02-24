ba7fe5cd235e55384847e37a9bb3b3ce1db36006
DaKnOb <daknob.mac@gmail.com>
2016-02-24 20:50:25 +0200

    Add comments to code

--- pimage.py
+++ pimage.py
@@ -1,13 +1,17 @@
 #!bin/python
 
+# Load Libraries
 from PIL import Image
 from random import randint
 
+# Create New Image -- Black by default
 img = Image.new( 'RGB', (255,255), "black")
 pixels = img.load()
 
+# Randomize all pixels
 for i in range(img.size[0]):
     for j in range(img.size[1]):
         pixels[i,j] = (randint(0,255), randint(0,255), randint(0,255))
 
+# Open the image
 img.show()
