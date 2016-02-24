ff2adf87592bac376f3033fb9ce66c1ed7698e1b
DaKnOb <daknob.mac@gmail.com>
2016-02-24 20:57:56 +0200

    Open Pi File in PiMage

--- pimage.py
+++ pimage.py
@@ -8,6 +8,14 @@ from random import randint
 img = Image.new( 'RGB', (255,255), "black")
 pixels = img.load()
 
+# Open file
+pi = open("pi/100000.txt", "r")
+piv = pi.read()
+pi = pi.close()
+
+# Remove 3. -- We want the decimals!
+piv = piv[2:]
+
 # Randomize all pixels
 for i in range(img.size[0]):
     for j in range(img.size[1]):
